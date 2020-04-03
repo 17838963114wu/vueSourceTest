e817b3abddbbd3b74916a6c4cd3b2b40ae8554ef
wumaosheng <1783896314@qq.com>
2020-04-03 18:29:03 +0800

    get all childNodes

--- vue/Mvue.js
+++ vue/Mvue.js
@@ -15,7 +15,9 @@ class Compile{
         // 获取文档碎片对象，放入内存中会减少页面的回流和重绘
         const fragment = this.node2Fragment(this.el);
         console.log(fragment);
-        //2.追加子元素到跟元素
+        this.compile(fragment);
+        //3.追加子元素到跟元素
+        this.el.appendChild(fragment);
     }
     isElementNode(node){
         return node.nodeType === 1;
@@ -27,6 +29,22 @@ class Compile{
             f.appendChild(firstChild);
         }
         return f;
+    }
+    compile(fragment){
+        //获取子节点
+        const childNodes =  fragment.childNodes;
+        // console.log(childNodes)
+        [...childNodes].forEach(child=>{
+            if(this.isElementNode(child)){
+                console.log('元素节点',child)
+            }else{
+                console.log('文本节点', child)
+            }
+        })
+        if(child.childNodes && child.childNodes.length){
+            // 递归，获取跟元素下所有子节点
+            this.compile(child);
+        }
     }
 
 }
